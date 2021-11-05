c909e85b7080ce4fa376b3b726ba48ea8fc8676a
Mikhail Klyushin <klyuhsinmisha@gmail.com>
2021-11-06 02:59:32 +0300

    prevent slab list nodes allocations

--- internal/slab_list.go
+++ internal/slab_list.go
@@ -2,18 +2,21 @@ package internal
 
 import (
 	"context"
+	"sync"
 
 	"golang.org/x/sync/semaphore"
 )
 
 type Node struct {
-	slab []byte
-	next *Node
+	slab  []byte
+	left  *Node
+	right *Node
 }
 
 type SlabList struct {
-	sem  *semaphore.Weighted
-	head *Node
+	sem *semaphore.Weighted
+	mux sync.Mutex
+	ptr *Node
 }
 
 func NewSlabList(capacity int, slabSize int) *SlabList {
@@ -21,17 +24,31 @@ func NewSlabList(capacity int, slabSize int) *SlabList {
 		sem: semaphore.NewWeighted(int64(capacity)),
 	}
 
+	var prev *Node
+
 	for i := 0; i < capacity; i++ {
-		sl.head = &Node{make([]byte, slabSize), sl.head}
+		sl.ptr = &Node{make([]byte, slabSize), nil, prev}
+
+		if prev != nil {
+			prev.left = sl.ptr
+		}
+
+		prev = sl.ptr
 	}
+
+	sl.ptr = &Node{nil, nil, prev}
+	prev.left = sl.ptr
 
 	return sl
 }
 
 func (sl *SlabList) Push(slab []byte) {
-	sl.sem.Acquire(context.Background(), 1)
+	sl.mux.Lock()
 
-	sl.head = &Node{slab, sl.head}
+	sl.ptr.slab = slab
+	sl.ptr = sl.ptr.left
+
+	sl.mux.Unlock()
 
 	sl.sem.Release(1)
 }
@@ -39,10 +56,12 @@ func (sl *SlabList) Push(slab []byte) {
 func (sl *SlabList) Pop() []byte {
 	sl.sem.Acquire(context.Background(), 1)
 
-	slab := sl.head.slab
-	sl.head = sl.head.next
+	sl.mux.Lock()
 
-	sl.sem.Release(1)
+	sl.ptr = sl.ptr.right
+	slab := sl.ptr.slab
+
+	sl.mux.Unlock()
 
 	return slab
 }
